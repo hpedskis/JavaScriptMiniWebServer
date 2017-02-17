d26784b37662aac3dda2534de79fd89837d6081f
hpedskis <hannah.pedersen.15@gmail.com>
2017-02-17 14:20:58 -0500

    have warmUp working

--- src/warmUp.js
+++ src/warmUp.js
@@ -1,1 +1,29 @@
 // warmUp.js
+const net = require('net');
+const HOST = '127.0.0.1';
+const PORT = 8080;
+
+
+const server = net.createServer((sock) => {
+        console.log('got connection from ' + sock.remoteAddress + ':' + sock.remotePort);
+        /*/
+        sock.on('data', function(binaryData) {
+            console.log('got data\n=====\n' + binaryData);
+            sock.write('HTTP/1.1 200 OK\r\n Content-Type: text/html \r\n <h1>Check out my fancy header!</h1>');
+            sock.end();
+
+        });
+        /*/
+    sock.on('data', function(binaryData) {
+        console.log('got data\n=====\n' + binaryData);
+        sock.write('HTTP/1.1 200 OK \r\n\r\n <h1><em>FINALLY IT WORKS</em></h1>');
+
+        // uncomment me if you want the connection to close
+        // immediately after we send back data
+        sock.end();
+    });
+
+
+});
+
+server.listen(PORT, HOST);
